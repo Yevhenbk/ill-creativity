b2679b45f225eade197e6efd8b6958026ddf9fd8
Yevhenbk <balagutrak.jenia@gmail.com>
2023-06-09 17:43:16 +0200

    Responsive Navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: FC<Props> = (props) => {
     <div
       className='w-[100%] h-20 bg-black border-b-[1px] fixed
       border-[#2c2c2d] flex flex-row justify-around items-center
-      z-50'
+      z-50 max-[700px]:justify-between max-[700px]:px-8'
     >
       {props.children}
     </div>
--- src/components/organisms/Navbarmenu.tsx
+++ src/components/organisms/Navbarmenu.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react'
+'use client'
+
+import React, { FC, useState } from 'react'
 import Text from '@atoms/Text'
 import Navbar from './Navbar'
 import Link from 'next/link'
@@ -6,23 +8,33 @@ import {
   menuItems, Props as MenuItemsProps 
   } from '@utils/MenuItems'
 import { 
-  AiOutlineShopping, AiOutlineSearch
+  AiOutlineShopping, AiOutlineSearch, AiOutlineMenu
  } from 'react-icons/ai'
+import { GrClose } from 'react-icons/gr'
 
 interface Props {}
 
 const Navbarmenu: FC<Props> = ({}) => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <>
       <Navbar>
         <div className='flex flex-row justify-around
-        w-[25rem] items-center'>
+        w-[25rem] items-center max-[700px]:w-auto relative'>
           <Link href='/'>
             <Text intent='primary' uppercase={true} size='large'
             italic={true} bold={true}>
               <h2>/ Logo</h2>
             </Text>
           </Link>
+          <div className='flex justify-around w-[70%] items-center
+          max-[700px]:hidden'>
           {menuItems.map((item: MenuItemsProps) => (
             <Link href={item.title}>
               <Text intent='primary' uppercase={true} size='extrasmall'>
@@ -30,9 +42,10 @@ const Navbarmenu: FC<Props> = ({}) => {
               </Text>
             </Link>
           ))}
+          </div>
         </div> 
         <div className='flex flex-row justify-around
-        w-[15rem] items-center'>
+        w-[15rem] items-center max-[700px]:hidden max-[700px]:w-auto'>
           <Link href='/support'>
             <Text intent='secondary' size='extrasmall'>
               <p className='hover:text-white duration-150
@@ -41,6 +54,23 @@ const Navbarmenu: FC<Props> = ({}) => {
           </Link>
           <AiOutlineSearch className='text-[1.75rem] text-white' />
           <AiOutlineShopping className='text-[1.75rem] text-white' />
+        </div>
+        <button onClick={toggleMenu} className='min-[700px]:hidden'>
+          <AiOutlineMenu className='text-white text-2xl min-[700px]:hidden' />
+        </button>
+        <div className={`bg-white absolute right-0 top-0 duration-300 shadow-md pt-28
+        w-[100vw] h-[100vh] min-[700px]:hidden flex flex-col justify-start items-center
+        gap-6 ${isMenuOpen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-full'}`}>
+          <button onClick={toggleMenu}>
+            <GrClose className='absolute top-8 right-8 text-2xl text-black' />
+          </button>
+          {menuItems.map((item: MenuItemsProps) => (
+            <Link href={item.title} className='w-[70%] text-right'>
+              <Text intent='teritary' uppercase={true} size='large'>
+                <p className='underline-on-hover'>{item.name}</p>
+              </Text>
+            </Link>
+          ))}
         </div>
       </Navbar>
       <div className='bg-black h-[2.4rem] flex justify-center items-center
